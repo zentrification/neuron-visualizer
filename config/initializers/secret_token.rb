ca9a7ab774525d5e6f7f257ede660348a3c3b6e5
Chris Dahms <zentrification@gmail.com>
2012-01-18 20:58:17 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeuronRedux::Application.config.secret_token = 'a33dcf410959af56494566ba0d849d16a47ab8ea23116153a579956575083722b1ba918695be81d19b7c53a86de10d9f6580de3d879b4974ec7ee4c243173bdd'
