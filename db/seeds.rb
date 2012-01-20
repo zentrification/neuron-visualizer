3bd045d93fc3cc51ce4626437cb331735c63e97c
Chris Dahms <zentrification@gmail.com>
2012-01-19 23:40:33 -0500

    added object id display on index actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,11 @@ neurons = Neuron.all
 (1..5).to_a.each do |x|
   a = PresynapticTerminal.new :label => 'axon' + x.to_s
   a.neuron = neurons.sample
+  a.notes = 'hihihi' if rand(2) == 0
   a.save
   a = PostsynapticTerminal.new :label => 'dendrite' + x.to_s
   a.neuron = neurons.sample
+  a.notes = 'bibbi' if rand(2) == 0
   a.save
 end
 post = PostsynapticTerminal.all
@@ -32,5 +34,6 @@ pre = PresynapticTerminal.all
   a.z = rand(1000) + rand(1000)
   a.presynaptic_terminal = pre.sample
   a.postsynaptic_terminal = post.sample
+  a.notes = 'moooo' if rand(2) == 0
   a.save
 end
